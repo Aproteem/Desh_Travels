8df092924fc46112c898da3349f13609d41e4a0c
Aproteem Majumder <axm220330@utdallas.edu>
2026-07-30 00:10:51 -0500

    added recommendation feature

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,7 +1,7 @@
-let data;
+const search_btn = document.getElementById("search_btn");
 
 
-async function getData() {
+async function getData(str) {
     const url = "./travel_recommendation_api.json";
     try {
         const response = await fetch(url);
@@ -9,10 +9,40 @@ async function getData() {
             throw new Error(`Response status: ${response.status}`);
         }
         const result = await response.json();
-        console.log(result);
+        showdata(result[str]);
     } catch (error) {
         console.error(error.message);
     }
 }
 
-getData()
+function showdata(array) {
+    array.forEach(detail => {
+        const imgurl = detail.imageUrl;
+        const name = detail.name;
+        const desc = detail.description;
+
+        const resultdiv = document.getElementById("search_res");
+
+        const newChild = document.createElement("div");
+        const html = `<img src="${imgurl}"><br><h1>${name}</h1><br><p>${desc}</p>`;
+        newChild.innerHTML = html;
+        resultdiv.appendChild(newChild);
+
+
+
+    })
+}
+
+function search() {
+    const searchterm = document.getElementById("searchstr");
+
+    if (searchterm.value.toLowerCase().includes("beach")) {
+        getData("beaches");
+    } else if (searchterm.value.toLowerCase().includes("temple")) {
+        getData("temples");
+    } else if (searchterm.value.toLowerCase().includes("countr")) {
+        getData("countries");
+    }
+}
+
+search_btn.addEventListener("click", search);
